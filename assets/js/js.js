d3d232c9effca68f1ab8d74ceb5a7779832cdb4e
Don Bedwell <timetourst@gmail.com>
2018-06-20 15:55:14 -0400

    Update js.js

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,24 +1,17 @@
 //declare variables
 var arrCurrRound = [];
-var arrCurrRnd = [];
 var questionAnswered = false;
 var questionsCorrect = 0;
 var questionsIncorrect = 0;
 var questionNum = 0;
 var gameStarted = false;
+var gameLength = 10;
+var tn;
 
-engage();
-
-$('#starter').click(function(){
-	// game started - set flag
-	gameStarted = true;
-});	
-
-function goAgain(){
+function resetGame(){
 	// clear variables
 	gameStarted = true;
 	arrCurrRound = [];
-	arrCurrRnd = [];
 	questionAnswered = false;
 	questionsCorrect = 0;
 	questionsIncorrect = 0;
@@ -29,53 +22,36 @@ function goAgain(){
 
 function engage(){
 	// wait until game starts
-	var tm = setInterval(function(){
-		if(gameStarted){
-			// clear game wait loop
-			clearInterval(tm);
-			//create the DOM elements for the game
-			initGameboard();
-			playTheGame(questionNum);
-		}
-	});
+	if(gameStarted){
+		// clear game wait loop
+		// create the DOM elements for the game
+		initGameboard();
+		playTheGame(questionNum);
+	}
 }
 
-//initial gameboard setup
+//initial game-board setup
 function initGameboard(){
-	// get 10 questions from the all question array
-	for (var i = 0; i < 10; i++){
+	// get questions from the all question array
+	for (var i = 0; i < gameLength; i++){
 		// get a random question object from the question array
-		var qPick = Math.floor((Math.random() * arrAllQuestions.length));
-		
+		var qPick = arrAllQuestions[Math.floor((Math.random() * arrAllQuestions.length))];
 		// error check: check if question has already been added to current round
-		if (arrCurrRnd.indexOf(qPick) > -1){
+		if (arrCurrRound.indexOf(qPick) > -1){
 			// try again
 			i--;
 		}else{
 			// add it to arrCurrRound
-			arrCurrRnd.push(qPick); console.log(arrCurrRnd);
+			arrCurrRound.push(qPick); 
+			//console.log(arrCurrRound);
 		}
-	}
-	// load ten questions from the arrAllQuestions array
-	for(var i = 0; i < arrCurrRnd.length; i++){
-		var objQuest = {
-			question:       arrAllQuestions[arrCurrRnd[i]].question,
-			distractor1:    arrAllQuestions[arrCurrRnd[i]].distractor1,
-			distractor2:    arrAllQuestions[arrCurrRnd[i]].distractor2,
-			distractor3:    arrAllQuestions[arrCurrRnd[i]].distractor3,
-			distractor4:    arrAllQuestions[arrCurrRnd[i]].distractor4,
-			correctAnswer:  arrAllQuestions[arrCurrRnd[i]].correctAnswer,
-			questionPic:    arrAllQuestions[arrCurrRnd[i]].questionPic,
-			questionPicAlt: arrAllQuestions[arrCurrRnd[i]].questionPicAlt
-		}
-		arrCurrRound.push(objQuest);
 	}
 	// clear and append content to gameboard divs
 	$('#gameboard').empty().append('<div class="row"><div id="question" class="col col-md-7"></div><div id="countdown" class="col col-md-5">&nbsp;</div><div id="question-img" class="col col-md-5 text-center"></div></div>');
 	// clear and append question/distractor divs
 	$('#question').empty().append('<div class="row"><div id="questionStem" class="col col-md-12"></div></div><div class="row"><div id="distractors" class="col col-md-12"></div><div class="row"><div id="feedback" class="col col-md-12"></div></div></div>');
 }
-var tn;
+
 function playTheGame(i){
 	questionAnswered = false;
 	// set the question
@@ -126,7 +102,7 @@ function playTheGame(i){
 function endGame(){
 	gameStarted = false;
 	var pct = (questionsCorrect/arrCurrRound.length)*100;
-	$('#gameboard').empty().html('<h1>GAME OVER</h1><p>You got ' + questionsCorrect + ' correct and ' + questionsIncorrect + ' incorrect!</p><p>A ' + pct + '% Rating</p><button id="starter" class="btn btn-lg btn-success center-block" onclick="goAgain()">TRY AGAIN</button>');
+	$('#gameboard').empty().html('<h1>GAME OVER</h1><p>You got ' + questionsCorrect + ' correct and ' + questionsIncorrect + ' incorrect!</p><p>A ' + pct + '% Rating</p><button id="starter" class="btn btn-lg btn-success center-block" onclick="resetGame()">PLAY AGAIN</button>');
 }
 
 function checkAnswer(r){
@@ -156,3 +132,8 @@ function recheck(){
 
 // goes nowhere does nothing (it's a Star Trek joke)
 function gndn(){}
+
+// let's play the game!
+$('#starter').click(function(){
+	resetGame();
+});
